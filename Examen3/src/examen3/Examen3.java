17b95acee222c7f41b0da8c512c32f85dabb6bc6
elton <elton@elton-Satellite-C55-B>
2018-10-08 15:00:19 -0300

    Examen3raparte

--- Examen3/src/examen3/Examen3.java
+++ Examen3/src/examen3/Examen3.java
@@ -47,21 +47,21 @@ public class Examen3 {
   
         //Creamos dos objetos con la primera posicion de cada array
         Serie serieMayor=listaSeries[0];
-        Peliculas videojuegoMayor=listaPeliculas[0];
+        Peliculas PeliculaMayor=listaPeliculas[0];
   
         //Recorremos el array desde la posicion 1 (no 0), comparando el mayor con las posiciones del array
         for(int i=1;i<listaSeries.length;i++){
             if(listaSeries[i].compareTo(serieMayor)==Serie.MAYOR){
                 serieMayor=listaSeries[i];
             }
-            if(listaPeliculas[i].compareTo(videojuegoMayor)==Peliculas.MAYOR){
-                videojuegoMayor=listaPeliculas[i];
+            if(listaPeliculas[i].compareTo(PeliculaMayor)==Peliculas.MAYOR){
+                PeliculaMayor=listaPeliculas[i];
             }
   
         }
   
         //Mostramos toda la informacion de peliculas y serie mayor
-        System.out.println(videojuegoMayor);
+        System.out.println(PeliculaMayor);
         System.out.println(serieMayor);
     }
     }
